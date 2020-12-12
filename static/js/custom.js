01bd387ac794cce34b387cb7d84a7e2360accba0
Sebastian Cerda <scerda@dts.lat>
2020-12-12 11:30:30 -0300

    last

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     $('#nav li').click(function(e) {
-        alert($(this).addClass("acive-li-nav"));
+        $('#nav li').removeClass("acive-li-nav");
+        $(this).addClass("acive-li-nav");
     });
 });
+$(document).ready(function() {
+    $('.login-form input').addClass('form-control')
+    $('.login-form button').addClass('text-center')
+    $('.producto_edit input').addClass('form-control-file')
+    $('.producto_nuevo input').addClass('form-control-file')
+ 
+    
+})
